dd875bdc5105c20eb73f4374cd0de6b2058e069d
Brian Thomason <brian.r.thomason@gmail.com>
2017-03-07 18:47:29 -0500

    begin implementation of tournament service.

---
+++ AegisBot/Implementations/TournamentService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Discord;
+
+namespace AegisBot.Implementations
+{
+    public class TournamentService : AegisService
+    {
+        public override List<CommandInfo> CommandList { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override string CommandDelimiter { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override List<ulong> Channels { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override string HelpText { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        internal override DiscordClient Client { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override void HandleEvents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task RunCommand(MessageEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task RunCommand(UserEventArgs e, string command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
